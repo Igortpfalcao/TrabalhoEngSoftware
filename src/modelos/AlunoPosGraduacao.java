d09d59fe9c02c48258d99d6134903f3ae22fe386
thiagodias19 <thiagodiasfla16@gmail.com>
2025-07-11 19:42:28 -0300

    Update AlunoPosGraduacao.java

--- src/modelos/AlunoPosGraduacao.java
+++ src/modelos/AlunoPosGraduacao.java
@@ -1,6 +1,6 @@
-package main.java.com.biblioteca.modelos;
+package modelos;
 
-import main.java.com.biblioteca.enums.TipoUsuario;
+import enums.TipoUsuario;
 
 public class AlunoPosGraduacao extends Usuario {
     
